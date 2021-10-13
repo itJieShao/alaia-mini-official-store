8626cb69cbfc36a052a864400ac65f8a4aa48d69
尤金金 <jinjin.you@baozun.com>
2021-10-13 16:48:48 +0800

    搜索

--- src/subPackages/search/pages/components/searchDefault.vue
+++ src/subPackages/search/pages/components/searchDefault.vue
@@ -10,24 +10,19 @@
       </view>
     </view>
     <view class="guess-like">
-      <text class="title">猜你喜欢</text>
-      <scroll-view class="guess-list" scroll-x="true">
-        <view class="item" v-for="(item,index) in guessLike" :key="index">
-          <image class="cover" :src="item.cover" mode="aspectFill"></image>
-          <view class="title">{{item.title}}</view>
-          <view class="price">{{item.price}}</view>
-        </view>
-      </scroll-view>
+      <recently-like-products />
     </view>
   </view>
 </template>
 
 <script>
 import searchInput from '@/components/searchInput';
+import RecentlyLikeProducts from '@/pages/shoppingCar/components/RecentlyLikeProducts'
 
 export default {
   components: {
     searchInput,
+    RecentlyLikeProducts,
   },
   props: {
     hotWords: {
@@ -88,47 +83,6 @@ export default {
 }
 .guess-like {
   margin-top: rpx(30);
-  padding-left: rpx(16);
-  .title {
-    font-family: PingFangSC, PingFangSC-Medium;
-    font-size: rpx(14);
-    font-weight: 500;
-    line-height: rpx(20);
-    letter-spacing: 2px;
-    color: #1d1d1d;
-  }
-  .guess-list {
-    margin-top: rpx(30);
-    white-space: nowrap;
-    .item {
-      display: inline-block;
-      width: rpx(165);
-      margin-right: rpx(8);
-      text-align: center;
-      .cover {
-        width: 100%;
-        height: rpx(227);
-      }
-      .title {
-        font-family: PingFangSC, PingFangSC-Regular;
-        font-size: rpx(12);
-        font-weight: 400;
-        line-height: rpx(14);
-        height: rpx(28);
-        margin-top: rpx(15);
-        white-space: initial;
-        color: #1d1d1d;
-      }
-      .price {
-        font-family: Lato, Lato-Regular;
-        font-size: rpx(12);
-        font-weight: 400;
-        line-height: rpx(14);
-        margin-top: rpx(4);
-        color: #1d1d1d;
-      }
-    }
-  }
 }
 
 </style>
--- src/subPackages/search/pages/components/searchNoRes.vue
+++ src/subPackages/search/pages/components/searchNoRes.vue
@@ -1,10 +1,6 @@
 <template>
   <view>
-    <view class="search-res">
-      <text decode class="search-res-title">搜索&nbsp;&nbsp;“{{keyword.name}}”</text>
-      <text class="search-res-num">共0个结果</text>
-      <text class="search-res-font">很抱歉，我们未找到与“{{keyword.name}}”匹配的内容，请尝试其他关键词。</text>
-    </view>
+    <view class="search-res">抱歉，没有找到任何关于“{{keyword.name}}”的结果 </view>
     <search-input @getProduct="getProduct" />
     <view class="no-list">
       <recently-like-products />
@@ -13,129 +9,45 @@
 </template>
 
 <script>
-  import searchInput from '@/components/searchInput';
-  import RecentlyLikeProducts from '@/pages/shoppingCar/components/RecentlyLikeProducts'
-  export default {
-    components: {
-      searchInput,
-      RecentlyLikeProducts
+import searchInput from '@/components/searchInput';
+import RecentlyLikeProducts from '@/pages/shoppingCar/components/RecentlyLikeProducts'
+
+export default {
+  components: {
+    searchInput,
+    RecentlyLikeProducts,
+  },
+  props: {
+    keyword: {
+      type: Object,
+      default: {},
     },
-    props: {
-      keyword: {
-        type: Object,
-        default: {},
-      }
+  },
+  methods: {
+    getProduct (params) {
+      this.$emit('getProduct', params, 1);
     },
-    methods: {
-      getProduct(params) {
-        this.$emit("getProduct", params, 1);
-      },
-    },
-  }
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-  $textColor:#1D1D1D;
+@import '@/styles/utilities.scss';
+.no-list {
+  margin-top: rpx(54);
+  padding-bottom: calc(100rpx + var(--safe-area-inset-bottom));
+  color: #1d1d1d;
+}
+.search-res {
+  font-family: PingFangSC, PingFangSC-Medium;
+  font-size: rpx(14);
+  font-weight: 500;
+  line-height: rpx(14);
+  margin-top: rpx(27);
+  padding: 0 rpx(16);
+  text-align: left;
+  letter-spacing: 1px;
+  color: #272727;
+}
 
-  .no-list {
-    margin-top: 100upx;
-    text-align: center;
-    color: $textColor;
-    padding-bottom: calc(100rpx + var(--safe-area-inset-bottom));
-  }
-
-  .search-res {
-    text {
-      display: block;
-      font-family: 'HiraginoSansGB';
-    }
-
-    .search-res-title {
-      width: 680upx;
-      padding-top: 50upx;
-      margin: 0 auto;
-      font-size: 32upx;
-      color: #1D1D1D;
-    }
-
-    .search-res-num {
-      width: 680upx;
-      margin: 30upx auto 0;
-      font-size: 24upx;
-      color: #666;
-    }
-
-    .search-res-font {
-      width: 588upx;
-      margin: 0 auto;
-      color: #0e0e0e;
-      font-size: 24upx;
-      letter-spacing: 1.72upx;
-      line-height: 48upx;
-      text-align: center;
-      padding: 60upx 0 40upx;
-    }
-  }
-
-  .goods-recommend {
-    width: 700upx;
-    margin: 60upx auto;
-
-    .goods-type {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-
-      .goods-type-icon {
-        font-size: 28upx;
-        padding: 3upx 20upx;
-        border-bottom: 12upx solid #E3F0EA;
-        letter-spacing: 5upx;
-      }
-
-      .goods-type-name {
-        font-size: 34upx;
-        color: #0e0e0e;
-        margin-top: 20upx;
-      }
-    }
-
-    .goods-con {
-      display: flex;
-
-      .arrow {
-        width: 40upx;
-        margin-top: 150upx;
-      }
-    }
-
-    .goods-swiper {
-      flex: 1;
-      height: 500upx;
-
-      swiper-item {
-        display: flex;
-
-        .goods-swiper-item {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          flex: 1;
-
-          image {
-            margin-top: 50upx;
-            width: 250upx;
-            height: 250upx;
-          }
-
-          text {
-            display: block;
-            margin-top: 20upx;
-            color: #1d1d1d;
-            text-align: center;
-          }
-        }
-      }
-    }
-  }
 </style>
