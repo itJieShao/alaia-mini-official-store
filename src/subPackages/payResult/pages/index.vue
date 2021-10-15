85c14ca2438cd4f306bcc6510c6ef2b5cf49bd3f
wulei <wulei@baixing.com>
2021-10-15 11:23:36 +0800

    feat: 1. 新增订单总和组件。2. 对结算页和支付页进行替换。

--- src/subPackages/payResult/pages/index.vue
+++ src/subPackages/payResult/pages/index.vue
@@ -82,24 +82,7 @@
         </view>
       </view>
       <!-- 订单金额汇总 -->
-      <view class="order-info">
-        <view class="info-item">
-          <view class="info-title">商品金额</view>
-          <view class="value"
-            >{{ get(orderInfo, 'amount.amount') | currency}} </view
-          >
-        </view>
-        <view class="info-item">
-          <view class="info-title">运费</view>
-          <view class="value">免运费</view>
-        </view>
-        <view class="info-item">
-          <view class="info-title">总计</view>
-          <view class="value"
-            >{{ get(orderInfo, 'amount.amount') | currency}} </view
-          >
-        </view>
-      </view>
+      <OrderAmountInfo :orderAmount="get(orderInfo, 'amount.amount')" />
       <!-- 处理按钮 -->
       <view class="fixed-button-wrap">
         <view class="button-container">
@@ -112,14 +95,15 @@
 </template>
 <script>
 import { mapActions, mapMutations } from 'vuex';
-import OrderProductItem from '@/components/orderProductItem';
+import OrderProductItem from '@/components/al-orderProductItem';
+import OrderAmountInfo from '@/components/al-orderAmountInfo';
 import customButton from '@/components/button/normal.vue';
 import navBarHeight from '@/components/common/navBarHeight';
 import { get } from '@/utils/utilityOperationHelper';
 import utils from '@/utils/utils';
 
 export default {
-  components: { OrderProductItem, customButton },
+  components: { OrderProductItem, OrderAmountInfo, customButton },
   name: 'PayResult',
   mixins: [navBarHeight],
   data() {
@@ -260,221 +244,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.pay-result-container {
-  .container {
-    padding-bottom: calc(128rpx + var(--safe-area-inset-bottom));
-    padding-top: 178rpx;
-    background-color: #f9f9f9;
-    .service-btn {
-      display: inline;
-      background: transparent;
-      line-height: 1;
-      text {
-        margin-left: 4rpx;
-        font-size: 28rpx;
-        color: #8e8e8e;
-        text-decoration: underline;
-      }
-    }
-    .order-info {
-      padding: 56rpx 32rpx;
-      .info-item {
-        display: flex;
-        justify-content: space-between;
-        font-size: 28rpx;
-        color: #000000;
-        &:nth-child(1) {
-          padding-bottom: 32rpx;
-        }
-        &:nth-child(2) {
-          padding-bottom: 48rpx;
-          border-bottom: 2rpx solid #bbbbbb;
-        }
-        &:nth-child(3) {
-          padding-top: 56rpx;
-        }
-      }
-    }
-  }
-  .result-info {
-    padding: 56rpx 32rpx !important;
-    text-align: center;
-    margin-bottom: 20rpx;
-    .result-icon {
-      margin-bottom: 32rpx;
-      image {
-        width: 60rpx;
-        height: 60rpx;
-      }
-      .icon-font {
-        font-size: 60rpx;
-      }
-    }
-    .result-msg {
-      margin-bottom: 48rpx;
-      font-size: 32rpx;
-      color: #1d1d1d;
-    }
-    .result-tips {
-      font-size: 28rpx;
-      color: #8e8e8e;
-    }
-  }
-  .order-brief {
-    padding: 48rpx 32rpx !important;
-    margin-bottom: 20rpx;
-    .brief-item {
-      margin-bottom: 40rpx;
-      color: #1d1d1d;
-      font-size: 28rpx;
-      .label {
-        margin-right: 8rpx;
-      }
-      &-last {
-        margin: 0;
-      }
-    }
-  }
-
-  .last-product {
-    /deep/ .product-box {
-      padding: 0;
-      .item-box {
-        border: none;
-      }
-    }
-  }
-  .form-item-block {
-    background: #fff;
-    padding: 0 32rpx;
-    margin-bottom: 20rpx;
-    overflow: hidden;
-    &.distribution {
-      margin: 0;
-      .form-item-title {
-        height: 120rpx !important;
-        border: none;
-      }
-      .distribution-info {
-        font-size: 28rpx;
-        border-bottom: 2rpx solid #f4f4f4;
-        .addressee {
-          margin-bottom: 20rpx;
-          font-size: #1d1d1d;
-          .name {
-            margin-right: 30rpx;
-          }
-        }
-        .address-detail {
-          color: #616161;
-          margin-bottom: 40rpx;
-        }
-      }
-    }
-    .form-item-title {
-      display: flex;
-      height: 132rpx;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 2rpx solid #f4f4f4;
-      font-size: 32rpx;
-      color: #252525;
-      .title-left {
-        text {
-          font-size: 28rpx;
-        }
-      }
-      &.sub {
-        height: 120rpx;
-      }
-      &.center {
-        justify-content: center;
-      }
-      &.last {
-        border: none;
-      }
-      &.address-row {
-        height: auto;
-        min-height: 120rpx;
-        .address-info {
-          padding: 32rpx 0;
-          font-size: 28rpx;
-          color: #1b1b1b;
-          .phone-text {
-            color: #313131;
-            margin-left: 36rpx;
-          }
-          .address-detail {
-            margin-top: 32rpx;
-          }
-          .form-error {
-            color: #e4210d;
-            margin-top: 32rpx;
-          }
-        }
-      }
-      .icon-font {
-        margin-right: 12rpx;
-        color: #bbbbbb;
-        font-size: 32rpx;
-        &.icon-icon-dingwei,
-        &.icon-icon-youjiantou {
-          color: #1d1d1d;
-        }
-      }
-
-      .title-right {
-        font-size: 28rpx;
-      }
-    }
-    .text-28 {
-      font-size: 28rpx;
-    }
-    .form-item-label {
-      display: flex;
-      margin-bottom: 48rpx;
-    }
-    /deep/ .checkbox {
-      height: 32rpx;
-      margin-right: 20rpx;
-    }
-    .form-item-input {
-      // margin: 48rpx 0;
-      margin-bottom: 48rpx;
-      position: relative;
-      input {
-        height: 40px;
-        padding: 0 32rpx;
-        background: #f9f9f9;
-      }
-      .error-wrap {
-        position: absolute;
-        top: 100%;
-        left: 0;
-        /deep/ {
-          .form-error {
-            font-size: 24rpx;
-            margin-top: 8rpx;
-          }
-        }
-      }
-    }
-  }
-  .fixed-button-wrap {
-    position: fixed;
-    left: 0;
-    bottom:0;
-    width: 100%;
-    padding-bottom: var(--safe-area-inset-bottom);
-    background: #fff;
-    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
-    .button-container {
-      display: flex;
-      align-content: center;
-      justify-content: center;
-      height: 128rpx;
-      padding: 24rpx 0;
-    }
-  }
-}
+  @import './index';
 </style>
