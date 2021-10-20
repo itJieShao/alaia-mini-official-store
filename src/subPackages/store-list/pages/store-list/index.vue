e98722fea18897d30c44ed9fd8037025b50a3e72
unknown <jm018610@baozun.cn>
2021-10-20 17:55:33 +0800

    icon替换

--- src/subPackages/store-list/pages/store-list/index.vue
+++ src/subPackages/store-list/pages/store-list/index.vue
@@ -43,7 +43,7 @@
           <!-- 距离 -->
           <view class="store-distance">
             <view v-if="storeItem.distance" @click="toMap(storeItem)">
-              <text class="icon-font icon-icon-dingdian"></text>
+              <text class="icon-font icon-fangwei"></text>
               <text class="distance">{{storeItem.distance}}km</text>
             </view>
             <!-- <view class="distance" v-else>暂无</view> -->
