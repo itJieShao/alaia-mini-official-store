35e37064c82ef636b376dbe40a7eb925bb91b5cc
wulei <wulei@baixing.com>
2021-11-01 13:56:13 +0800

    feat: 完成帮助中心详情数据对接

--- src/subPackages/help/pages/help-detail/index.vue
+++ src/subPackages/help/pages/help-detail/index.vue
@@ -7,20 +7,17 @@
       </view>
     </view>
     <view class="help-detail-content">
-      <view>
-        <view class="title">关于正品</view>
-        <view class="content">我们向您保证ALAÏA精品店所售商品均为原装正品。</view>
-      <view>
-        <view>
-        <view class="title">关于签收</view>
-        <view class="content">ALAÏA唯一指定官方配送渠道为顺丰速递，我们将确保商品安全送至您的手中，并将为您的订单提供保险，以预防配送期间可能出现的丢失和意外损伤。 一旦您的商品送至指定地址并被本人签收，所购商品将不再受保险保护，如包裹送达时您无法收取，您可以提前联系在线顾问，我们将协助您解决。</view>
-      <view>
+      <view :key="i" v-for="helpContent, i in helpContentList">
+        <view class="title">{{ helpContent.title }}</view>
+        <view class="content">{{ helpContent.content }}</view>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
 import { getCmsContent } from '@/service/apis';
+import { parseCmsContent  } from '@/utils/cms';
 import { get } from '@/utils/utilityOperationHelper';
 
 export default {
@@ -31,14 +28,13 @@ export default {
       helpTitle: '', // 标题
       templateCode: '', // templateCode 值
       contentCode: '', // contentCode 值
-      helpContent: [], // 页面内容
+      helpContentList: [], // 页面内容
     }
   },
   onLoad(option) {
     this.helpTitle = option.name;
     this.templateCode = option.templateCode;
     this.contentCode = option.contentCode;
-    console.log(this.templateCode, this.contentCode)
     this.getHelpDetail();
   },
   // 阻止下拉刷新
@@ -47,21 +43,13 @@ export default {
   },
   methods: {
     async getHelpDetail() {
-      // try {
-      //   const res = await getCmsContent({
-      //     templateCode: this.templateCode,
-      //     contentCode: this.contentCode,
-      //   });
-      //   const cmsContent = JSON.parse(get(res, 'data.shop.cmsContent', null)) || {};
-      //   this.helpTitle = cmsContent.contentName;
-      //   console.log('cmsContent ===>', cmsContent);
-      //   const helpContent = get(cmsContent, 'zh_CN.content.modelContents', [])
-      //     .map((v) => ({ content: v.groupContents.content[0].fieldContents.content, type: v.groupContents.content[0].fieldContents.type, fontWeight: v.groupContents.content[0].fieldContents['special requirements'] == '["bold"]' ? 'bold': ''}))
-      //   console.log('helpContent ===>', helpContent);
-      //   this.helpContent = helpContent;
-      // } catch (error) {
-      //   console.error(error)
-      // }
+      try {
+        const res = await getCmsContent({ templateCode: this.templateCode, contentCode: this.contentCode });
+        const resData = get(res, 'data.shop.templateData');
+        this.helpContentList = parseCmsContent(resData, 'help_content_item', 'text_config');
+      } catch (error) {
+        console.error(error)
+      }
     },
   },
 }
